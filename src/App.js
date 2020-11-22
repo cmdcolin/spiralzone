f1153c30528a1e9f9c7bcc972c19f59366240887
Colin <colin.diesh@gmail.com>
2020-11-21 21:06:57 -0500

    Spiral zone

--- src/App.js
+++ src/App.js
@@ -1,57 +1,65 @@
 import { useRef, useEffect } from "react";
 import "./App.css";
 
-function f(t) {
-  return [t * Math.sin(t), t * Math.cos(t)];
-}
-
 //credit https://stackoverflow.com/questions/3417183/modulo-of-negative-numbers
 function wrap(i, i_max) {
   return ((i % i_max) + i_max) % i_max;
 }
+
+const rando = 20;
 function App() {
   const ref = useRef();
   useEffect(() => {
-    let t = 0;
     if (ref.current) {
       const ctx = ref.current.getContext("2d");
-      const color = ["#dde", "#dcc", "dde"];
 
-      // setInterval(() => {
-      //   ctx.fillStyle = color[t % color.length];
-      //   ctx.fillRect(
-      //     Math.random() * 1000 - 300,
-      //     Math.random() * 800 - 200,
-      //     Math.random() * 1000,
-      //     Math.random() * 800
-      //   );
-      //   t++;
-      // }, 400);
       function gen(colors, walk = [0.45, 0.4], initial = [0, 0]) {
         let pos = [...initial];
+        let t = 0;
+        let ct = 0;
         return () => {
-          ctx.fillStyle = colors[t % colors.length];
-          ctx.fillRect(pos[0], pos[1], Math.random() * 20, Math.random() * 20);
-          pos[0] = wrap(pos[0] + 20 * (Math.random() - walk[0]), 1800);
-          pos[1] = wrap(pos[1] + 20 * (Math.random() - walk[1]), 1000);
+          ctx.fillStyle = colors[ct % colors.length];
+          ctx.fillRect(
+            pos[0] + 500 * Math.cos(ct / 1000) * Math.sin(t),
+            pos[1] + 500 * Math.cos(ct / 1000) * Math.cos(t),
+            Math.random() * 200 * Math.sin(ct / 100),
+            Math.random() * 200 * Math.sin(ct / 100)
+          );
+          pos[0] = wrap(pos[0] + rando * (Math.random() - walk[0]), 1800);
+          pos[1] = wrap(pos[1] + rando * (Math.random() - walk[1]), 1000);
+          t += 0.01;
+          ct++;
         };
       }
       const walkers = [
-        gen(["blue", "navy"]),
+        gen(["blue", "navy"], [0.5, 0.5], [600, 600]),
         gen(["green", "darkgreen"], [0.5, 0.48], [400, 600]),
         gen(["red", "darkred"], [0.5, 0.5], [1000, 400]),
+        gen(["yellow", "orange"], [0.49, 0.5], [1000, 400]),
+        gen(["#f0f", "purple"], [0.49, 0.49], [1600, 300]),
+        gen(["#232", "black"], [0.49, 0.49], [200, 200]),
+        gen(["#249", "43a"], [0.49, 0.49], [900, 900]),
       ];
       setInterval(() => {
         walkers.forEach((walker) => walker());
-
-        t++;
       }, 10);
 
-      setTimeout(() => {
-        ctx.fillStyle = "black";
-        ctx.moveTo(...f(t));
-        ctx.lineTo(...f(t + 1));
-      });
+      setInterval(() => {
+        for (let i = 0; i < 3; i++) {
+          ctx.clearRect(
+            (Math.random() - 0.5) * 1800,
+            (Math.random() - 0.5) * 2000,
+            100,
+            200
+          );
+        }
+      }, 10);
+
+      // setTimeout(() => {
+      //   ctx.fillStyle = "black";
+      //   ctx.moveTo(...f(t));
+      //   ctx.lineTo(...f(t + 1));
+      // });
     }
   }, []);
   return (
